2e1bb6571f676f7790709ded6fc352d9285b8ec2
Dmytro-Kyianov <dkyianov@gmail.com>
2023-05-08 22:09:09 +0300

    all tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,6 @@
 const categoriesItemEl = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesItemEl.length}`)
 
-
-
-
 categoriesItemEl.forEach(number => {
     console.log(`Category: ${number.firstElementChild.textContent} Elements: ${number.lastElementChild.children.length}`)
     
--- js/task-08.js
+++ js/task-08.js
@@ -3,9 +3,17 @@ const formEl = document.querySelector('form');
 formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
-    
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return console.log("Please fill in all the fields!");
+    }
+    const dataArray = {
+        email: event.currentTarget.email.value,
+        password: event.currentTarget.password.value,
+    };
+    console.log(dataArray);
+    event.currentTarget.reset();
 }
 
-
-
-// console.log(formEl);
